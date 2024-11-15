307c23f7df551cb00fd7bfefa9144aa322bdce5a
goodluck1455 <goodluckstephen237@gmail.com>
2024-11-15 12:48:01 +0100

    deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -9,17 +9,37 @@ import react from '@vitejs/plugin-react'
 
 // import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom'], 
+//         },
+//       },
+//     },
+//   },
+// });
+
+
+
+
+
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'] // Split vendor libraries
-            // utilities: ['lodash', 'moment'],  Example for splitting utility libraries
+          vendor: ['react', 'react-dom'],
         },
       },
     },
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning if needed
   },
 });
+
+
 
