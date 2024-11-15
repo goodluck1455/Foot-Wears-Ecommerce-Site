820bb9ab5839419f02cd2d9e41bdc9b52816d377
goodluck1455 <goodluckstephen237@gmail.com>
2024-11-15 12:59:59 +0100

    deploument issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           vendor: ['react', 'react-dom'], 
-//         },
-//       },
-//     },
-//   },
-// });
-
-
-
-
-
-
+https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase chunk size warning if needed
-  },
-});
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
