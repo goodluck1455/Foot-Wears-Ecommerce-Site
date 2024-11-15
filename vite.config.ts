4fa2884025963f8f6fd0ae0bec5bd91a1395ed89
goodluck1455 <goodluckstephen237@gmail.com>
2024-11-15 12:18:22 +0100

    configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'] // Split vendor libraries
+            // utilities: ['lodash', 'moment'],  Example for splitting utility libraries
+        },
+      },
+    },
+  },
+});
+
